9016db11ca1d539a2e4658efd7fa7d8a395f5c6a
micavanco <micavanco@gmail.com>
2020-08-12 22:19:36 +0200

    feat(footer, header): add footer and header with bootstrap

--- apps/posts-app/src/app/app.module.ts
+++ apps/posts-app/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
